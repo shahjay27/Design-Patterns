018d2348f5b1bf1cf97cd1fea118e9beec01405b
jay shah <jayshah1996@gmail.com>
2023-10-21 19:59:32 -0400

    Thread safe singleton

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SingletonPattern;
+
+Task t1 = Task.Run(() => getPrintSingletoninstance("From Task1"));
+Task t2 = Task.Run(() => getPrintSingletoninstance("From Task2"));
+
+t1.Wait();
+t2.Wait();
+Console.ReadLine();
+
+void getPrintSingletoninstance(string value)
+{
+    Singleton s = Singleton.GetInstance(value);
+    Console.WriteLine(s.Value);
+}
