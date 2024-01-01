6c2c032a417bb0c8de4f62ad86de86a7a69f08d7
jay shah <jayshah1996@gmail.com>
2023-12-31 21:57:59 -0500

    Thread safe singleton remove debugger

--- SingletonPattern/Program.cs
+++ SingletonPattern/Program.cs
@@ -1,14 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SingletonPattern;
 
-Task t1 = Task.Run(() => getPrintSingletoninstance("From Task1"));
-Task t2 = Task.Run(() => getPrintSingletoninstance("From Task2"));
+Task t1 = Task.Run(() => getPrintSingletonInstance("From Task1"));
+Task t2 = Task.Run(() => getPrintSingletonInstance("From Task2"));
 
 t1.Wait();
 t2.Wait();
 Console.ReadLine();
 
-void getPrintSingletoninstance(string value)
+void getPrintSingletonInstance(string value)
 {
     Singleton s = Singleton.GetInstance(value);
     Console.WriteLine(s.Value);
